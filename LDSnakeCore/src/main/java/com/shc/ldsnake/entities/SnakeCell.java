da251e9c5126f73cf398999797e5a22425b90f32
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2016-12-12 13:19:17 +0530

    Refactor SnakeCell to remove if checks every frame

--- LDSnakeCore/src/main/java/com/shc/ldsnake/entities/SnakeCell.java
+++ LDSnakeCore/src/main/java/com/shc/ldsnake/entities/SnakeCell.java
@@ -7,6 +7,7 @@ import com.shc.silenceengine.input.Keyboard;
 import com.shc.silenceengine.math.Vector2;
 import com.shc.silenceengine.scene.components.SpriteComponent;
 import com.shc.silenceengine.scene.entity.Entity2D;
+import com.shc.silenceengine.utils.functional.UniCallback;
 
 import static com.shc.silenceengine.input.Keyboard.*;
 
@@ -20,24 +21,34 @@ public class SnakeCell extends Entity2D
     private SnakeCell behindCell;
     private Direction direction;
 
-    public SnakeCell(float x, float y, SpriteBatch batch)
+    private UniCallback<Float> updateHandler;
+
+    private SnakeCell()
     {
-        behindCell = null;
         velocity = new Vector2();
         direction = Direction.NONE;
+    }
+
+    public SnakeCell(float x, float y, SpriteBatch batch)
+    {
+        this();
 
         position.set(x, y);
         scale.set(0.5f);
+
+        updateHandler = this::updateHead;
 
         addComponent(new SpriteComponent(new Sprite(Resources.Textures.SNAKE_CELL), batch));
     }
 
     public SnakeCell(SnakeCell parent, SpriteBatch batch)
     {
-        behindCell = parent;
-        velocity = new Vector2();
-        direction = parent.direction;
+        this();
 
+        updateHandler = this::updateTail;
+
+        behindCell = parent;
+        scale.set(0.5f);
         position.set(parent.position);
 
         switch (parent.direction)
@@ -60,57 +71,59 @@ public class SnakeCell extends Entity2D
                 break;
         }
 
-        scale.set(0.5f);
-
         addComponent(new SpriteComponent(new Sprite(Resources.Textures.SNAKE_CELL), batch));
     }
 
     @Override
     protected void onUpdate(float deltaTime)
     {
-        if (behindCell == null)
+        updateHandler.invoke(deltaTime);
+        position.add(velocity);
+    }
+
+    private void updateHead(float deltaTime)
+    {
+        if (Keyboard.isKeyDown(KEY_W))
         {
-            if (Keyboard.isKeyDown(KEY_W))
-            {
-                velocity.set(0, -4);
-                direction = Direction.UP;
-            }
+            velocity.set(0, -4);
+            direction = Direction.UP;
+        }
 
-            if (Keyboard.isKeyDown(KEY_S))
-            {
-                velocity.set(0, 4);
-                direction = Direction.DOWN;
-            }
+        if (Keyboard.isKeyDown(KEY_S))
+        {
+            velocity.set(0, 4);
+            direction = Direction.DOWN;
+        }
 
-            if (Keyboard.isKeyDown(KEY_A))
-            {
-                velocity.set(-4, 0);
-                direction = Direction.LEFT;
-            }
+        if (Keyboard.isKeyDown(KEY_A))
+        {
+            velocity.set(-4, 0);
+            direction = Direction.LEFT;
+        }
 
-            if (Keyboard.isKeyDown(KEY_D))
-            {
-                velocity.set(4, 0);
-                direction = Direction.RIGHT;
-            }
+        if (Keyboard.isKeyDown(KEY_D))
+        {
+            velocity.set(4, 0);
+            direction = Direction.RIGHT;
+        }
+    }
+
+    private void updateTail(float deltaTime)
+    {
+        velocity.set(behindCell.position).subtract(position);
+
+        float distToBehind = velocity.length();
+        float followDistance = 32;
+
+        if (distToBehind > followDistance)
+        {
+            float tooFar = distToBehind - followDistance;
+            velocity.normalize().scale(tooFar);
         }
         else
-        {
-            velocity.set(behindCell.position).subtract(position);
+            velocity.set(0);
 
-            float distToBehind = velocity.length();
-            float followDistance = 32;
-
-            if (distToBehind > followDistance)
-            {
-                float tooFar = distToBehind - followDistance;
-                velocity.normalize().scale(tooFar);
-            }
-            else
-                velocity.set(0);
-        }
-
-        position.add(velocity);
+        direction = behindCell.direction;
     }
 
     private enum Direction
